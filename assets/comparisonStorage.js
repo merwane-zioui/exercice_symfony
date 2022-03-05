8d1c9f57780957e5f57cfbf9de5b150eea9d71e0
Merwane Zioui <merwane.zioui@gmail.com>
2022-03-05 19:48:08 +0100

    Update comparisonStorage.js

--- assets/comparisonStorage.js
+++ assets/comparisonStorage.js
@@ -1,4 +1,4 @@
-$(".remove-player").click(function () {
+$(document).on("click", "a.remove-player", function () {
     var $this = $(this).closest("li");
     $.ajax({
         url: '/removeFromComparator/'+$this.attr('id'),
